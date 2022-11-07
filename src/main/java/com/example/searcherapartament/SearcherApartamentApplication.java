0ba2a30448b407c89298585ac7058c51a4849bbc
hubert123 <hubert.antoniszyn98@gmail.com>
2022-11-07 13:21:55 +0100

    initial commit

---
+++ src/main/java/com/example/searcherapartament/SearcherApartamentApplication.java
@@ -0,0 +1,13 @@
+package com.example.searcherapartament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearcherApartamentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearcherApartamentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/searcherapartament/SearcherApartamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.searcherapartament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearcherApartamentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
