e5ca55eb11fe1d5261a6840e4d5c5b048ab56803
hubert123 <hubert.antoniszyn98@gmail.com>
2023-01-17 22:27:02 +0100

    Update olxTest

--- src/test/java/com/example/searcherapartament/offer/olxTest.java
+++ src/test/java/com/example/searcherapartament/offer/olxTest.java
@@ -1,6 +1,8 @@
 package com.example.searcherapartament.offer;
 
-import com.example.searcherapartament.offer.scraping.OfferSystem;
+import com.example.searcherapartament.scraper.Olx;
+import com.example.searcherapartament.scraper.OfferSystem;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,14 +13,36 @@ class olxTest {
     @Autowired
     OfferSystem offerSystem;
     @Autowired
-    com.example.searcherapartament.offer.scraping.olx olx;
+    Olx olx;
+
 
     @Test
-    void shouldReturn600AdditionalCost()
+    void shouldReturnBasicPrice()
     {
-        String url ="https://www.olx.pl/d/oferta/2-pokoje-osobna-kuchnia-duzy-balkon-CID3-IDRHQTT.html";
+        String url ="https://www.olx.pl/d/oferta/pokoj-jednoosobowy-do-wynajecia-w-cent-wroclawia-przy-ul-krynickiej-CID3-IDNHmDa.html";
+        assertEquals(700,olx.getBasicCost(url));
+    }
+    @Test
+    @DisplayName("0,01 zł")
+    void shouldReturnAdditionalCostswithSpecialCharacter()
+    {
+        String url ="https://www.olx.pl/d/oferta/2-pok-52-m2-3-5-km-od-rynku-wroclaw-karlowice-prestizowa-lokalizacja-CID3-IDR9nMP.html";
+        assertEquals(0,olx.getAdditionalCost(url));
+    }
+
+    @Test
+    void shouldReturnBasicPricewithWhiteSpace()
+    {
+        String url ="https://www.olx.pl/d/oferta/pokoj-dla-studenta-1200-zl-osiedle-ksiaze-wielkie-CID3-IDRB3PA.html";
+        assertEquals(1200,olx.getBasicCost(url));
+    }
+    @Test
+    @DisplayName("Cost with white space")
+    void shouldReturn1000AdditionalCost()
+    {
+        String url ="https://www.olx.pl/d/oferta/mieszkanie-3-pokojowe-56m-we-wroclawiu-rozkladowe-przy-ul-legnickiej-CID3-IDPcmZQ.html";
         System.out.println(olx.getAdditionalCost(url));
-        assertEquals(600,olx.getAdditionalCost(url));
+        assertEquals(1000,olx.getAdditionalCost(url));
     }
 
     @Test
